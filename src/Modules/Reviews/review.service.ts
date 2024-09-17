6d42f60ff335ed534a1df5d33cbc80b92b9b4129
smile995 <ah.smile995@gmai.com>
2024-09-17 12:40:46 +0600

    use transaction and rollback functionality in review module

--- src/Modules/Reviews/review.service.ts
+++ src/Modules/Reviews/review.service.ts
@@ -1,12 +1,29 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 
+import { startSession } from "mongoose";
+import { Movies } from "../Movies/movie.model";
 import { TReview } from "./review.interface"
 import { Review } from "./review.model"
 
-const createReview = async (movieId:any, payload: Partial<TReview>) => {
-  payload.movie = movieId
-  const result = await Review.create(payload);
-  return result
+
+const createReview = async (movieId: any, payload: Partial<TReview>) => {
+  const session = await startSession();
+  try {
+ session.startTransaction()
+    payload.movie = movieId
+    const result = (await Review.create(payload)).$session(session);
+    const totalReview = await Review.countDocuments({ movie: movieId })
+    await Movies.findOneAndUpdate([{ _id: movieId }, {
+      totalRating: totalReview
+    }, { new: true }, {session}])
+    await session.commitTransaction();
+  
+    return result
+  } catch (error) {
+    await session.abortTransaction();
+    await session.endSession();
+    throw new Error("Review not added successfully")
+  }
 }
 
 
